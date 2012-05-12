a7379cd62e6ceeb7b4286856d62c1ee0cd983efb
Nelson Melo <nmelo.cu@gmail.com>
2012-05-11 23:05:44 -0400

    Initial commit
    Signed-off-by: Nelson Melo <nmelo.cu@gmail.com>

---
+++ app/app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  app
+//
+//  Created by Nelson Melo on 5/11/12.
+//  Copyright (c) 2012 codemodlabs.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ app/app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  app
+//
+//  Created by Nelson Melo on 5/11/12.
+//  Copyright (c) 2012 codemodlabs.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
